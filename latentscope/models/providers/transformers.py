1753ce05ea3c4b74acd4db53128e88e8d3017ca5
JonathanNiesel <jonathan.niesel@web.de>
2024-04-29 11:17:57 +0200

    increase response length

--- latentscope/models/providers/transformers.py
+++ latentscope/models/providers/transformers.py
@@ -61,7 +61,7 @@ class TransformersChatProvider(ChatModelProvider):
         self.pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0", torch_dtype=torch.float32, device_map="cpu")
         self.encoder = self.pipe.tokenizer
 
-    def chat(self, messages, max_new_tokens=24):
+    def chat(self, messages, max_new_tokens=300):
         prompt = self.pipe.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
         outputs = self.pipe(prompt, max_new_tokens=max_new_tokens, do_sample=True, temperature=0.7, top_k=50, top_p=0.95)
         generated_text = outputs[0]["generated_text"]
